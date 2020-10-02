7f2a8850b9fb183f2268429aef777ea79f0b7cb6
ajg7 <ajgebara@gmail.com>
2020-10-02 10:06:39 -0500

    made index.js file

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ I need this code, but don't know where, perhaps should make some middleware, don
 
 Go code!
 */
+
+const server = require("express");
+
+const port = 8000;
+server.listen(port, () => {
+    console.log("It's WORKING!!!!!!!");
+})
